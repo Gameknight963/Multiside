e53b129d4880c224b954e1123267e2f6eaa16743
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-01-31 23:14:28 -0600

    ADD playerpositiondata.cs

---
+++ PlayerPositionData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MSZmultiplayer
+{
+    public class PlayerPositionData
+    {
+        public int playerId;
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public PlayerPositionData(int id, Vector3 pos, Quaternion rot)
+        {
+            playerId = id;
+            position = pos;
+            rotation = rot;
+        }
+    }
+}
