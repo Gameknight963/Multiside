860abe5469057c6f4b1f3f06b1b0555e08eebc9f
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-04-26 17:37:26 -0500

    add onproviderregistered event

--- MultiSide.shared/NetworkRegistry.cs
+++ MultiSide.shared/NetworkRegistry.cs
@@ -3,10 +3,12 @@
     public static class NetworkRegistry
     {
         public static INetworkProvider? Provider { get; private set; }
+        public static event Action<INetworkProvider>? OnProviderRegistered;
 
         public static void Register(INetworkProvider provider)
         {
             Provider = provider;
+            OnProviderRegistered?.Invoke(provider);
         }
     }
 }
