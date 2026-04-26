379fb68ff46f6288fb187ff6377f192d20039cef
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-04-26 15:37:56 -0500

    fix typos and change capitalization

--- MultiSide/Config.cs
+++ MultiSide/Config.cs
@@ -9,7 +9,7 @@ namespace MultiSide
         public readonly static AppSettings settings = new AppSettings
         {
             AppIdRealtime = "301df3f0-b282-4a33-b588-60e22cdf7d87",
-            AppVersion = "0.1.0",
+            AppVersion = "1.0.1",
             FixedRegion = "us",
             Protocol = ConnectionProtocol.Udp,
             NetworkLogging = LogLevel.Info
